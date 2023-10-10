9ca58b441506e6dd233b68007308eea5eb25d5b6
Eduardo Nelson Garcia Miranda <eddybolivia@gmail.com>
2023-10-10 00:50:47 +0000

    se aumento una funcion de nombre

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const resta = function(a, b) {
 
 const producto = (a, b) => a * b
 
+const nombreCompleto = (nombre) => `Hola, cómo estas ${nombre}`
+
 const resultado_suma = suma(2, 8)
 const resultado_resta = resta(1, 5)
 const resultado_producto = producto(3, 6)
@@ -19,3 +21,4 @@ const resultado_producto = producto(3, 6)
 console.log(resultado_suma)
 console.log(resultado_resta)
 console.log(resultado_producto)
+console.log(nombreCompleto('Carlos'));
